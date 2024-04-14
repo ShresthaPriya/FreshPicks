d7690ed0961a85b34190567a7468c316676cbaf6
Bibek Timalsina <bibektimalsina89@gmail.com>
2024-04-14 08:20:32 +0545

    debugged the email error for the browser

--- public/verification/email.js
+++ public/verification/email.js
@@ -1,17 +1,15 @@
-  // Function to validate email
-  function validateEmail() {
-    var emailField = document.getElementById("email_for_signup").value;
-    var emailerror = document.getElementById("email-error");
+function validateEmail() {
+  var emailField = document.getElementById("email_for_signup").value;
+  var emailerror = document.getElementById("errormsg_for_email");
   
-    if (emailField.trim() === "") {
-      emailerror.innerText = ""; // Clear the error message if email field is empty
-    } else if (!emailField.match(/^([a-zA-Z0-9_\-]+)@([a-zA-Z0-9_\-]+)\.([a-zA-Z]{2,5})$/)) {
-      emailerror.innerText = "Please enter a valid email address";
-    } else {
-      emailerror.innerText = ""; // Clear the error message if email is valid
-    }
+  if (emailField.trim() === "") {
+    emailerror.innerText = ""; // Clear the error message if email field is empty
+  } else if (!emailField.match(/^([a-zA-Z0-9_\-]+)@([a-zA-Z0-9_\-]+)\.([a-zA-Z]{2,5})$/)) {
+    emailerror.innerText = "Please enter a valid email address";
+  } else {
+    emailerror.innerText = ""; // Clear the error message if email is valid
   }
-  
-  // Add event listener to the email input field
-  document.getElementById("email_for_signup").addEventListener("input", validateEmail);
-  
+}
+
+// Add event listener to the email input field
+document.getElementById("email_for_signup").addEventListener("input", validateEmail);
