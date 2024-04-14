5855e9c5dd10a74fd5528f6b9684958a20f915a5
Elizasthaaa <np03cs4a220347@heraldcollege.edu.np>
2024-04-14 07:19:01 +0545

    Deleted the js code of backbutton in style.js file

--- public/style.js
+++ public/style.js
@@ -9,18 +9,3 @@ sign_up_btn.addEventListener("click", () => {
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
-
-document.addEventListener("DOMContentLoaded", function() {
-  const backButton = document.getElementById("backButton");
-
-  backButton.addEventListener("click", function(event) {
-    // Prevent the default behavior of the link
-    event.preventDefault();
-    
-    
-    // Redirect to the login_ad.html page after a short delay (for demonstration purposes)
-    setTimeout(function() {
-      window.location.href = backButton.getAttribute("href");
-    }, 500); // Adjust the delay time as needed
-  });
-});
